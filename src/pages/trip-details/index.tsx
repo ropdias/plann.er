b3421156dddf6b3713c8051bb072e2307ed5aded
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-14 16:27:10 -0300

    TripDetailsPage - Guests

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,10 +1,12 @@
 import {
   Calendar,
   CircleCheck,
+  CircleDashed,
   Link2,
   MapPin,
   Plus,
   Settings2,
+  UserCog,
 } from 'lucide-react';
 
 export function TripDetailsPage() {
@@ -122,7 +124,40 @@ export function TripDetailsPage() {
             </button>
           </div>
           <div className="w-full h-px bg-zinc-800" />
-          <div></div>
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Convidados</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Jessica White
+                  </span>
+                  <span className="block text-sm text-zinc-400 truncate">
+                    jessica.white44@yahoo.com
+                  </span>
+                </div>
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              </div>
+
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Dr. Rita Pacocha
+                  </span>
+                  <span className="block text-sm text-zinc-400 truncate">
+                    lacy.stiedemann@gmail.com
+                  </span>
+                </div>
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            </div>
+
+            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              <UserCog className="size-5" />
+              Gerenciar convidados
+            </button>
+          </div>
         </div>
       </main>
     </div>
