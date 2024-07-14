9deae35a2c13783b92af9664b11690e0e604090f
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-14 16:52:38 -0300

    creating component CreateActivityModal

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -8,8 +8,21 @@ import {
   Settings2,
   UserCog,
 } from 'lucide-react';
+import { useState } from 'react';
+import { CreateActivityModal } from './create-activity-modal';
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+    useState(false);
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true);
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false);
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -37,7 +50,10 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button
+              onClick={openCreateActivityModal}
+              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+            >
               <Plus className="size-5" />
               Cadastrar Atividade
             </button>
@@ -160,6 +176,12 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <CreateActivityModal
+          closeCreateActivityModal={closeCreateActivityModal}
+        />
+      )}
     </div>
   );
 }
