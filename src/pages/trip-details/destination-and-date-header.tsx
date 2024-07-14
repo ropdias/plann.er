56bac1ef411bcb70546d3371ab82b2294a4c5c0b
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-14 18:08:56 -0300

    global component Button: using tailwind-variants to create variant styles

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -17,7 +17,7 @@ export function DestinationAndDateHeader() {
 
         <div className="w-px h-6 bg-zinc-800" />
 
-        <Button>
+        <Button variant="secondary">
           Alterar local/data
           <Settings2 className="size-5" />
         </Button>
