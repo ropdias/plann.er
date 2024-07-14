bb25b6bd00d6904d6ef13442ab8d3cdecaddfdb1
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2024-07-14 16:22:18 -0300

    TripDetailsPage - Important Links

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,11 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react';
+import {
+  Calendar,
+  CircleCheck,
+  Link2,
+  MapPin,
+  Plus,
+  Settings2,
+} from 'lucide-react';
 
 export function TripDetailsPage() {
   return (
@@ -73,7 +80,50 @@ export function TripDetailsPage() {
             </div>
           </div>
         </div>
-        <div className="w-80"></div>
+        <div className="w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links importantes</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Reserva do AirBnB
+                  </span>
+                  <a
+                    href="#"
+                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                  >
+                    https://www.airbnb.com.br/rooms/104700011123123213213123121231231231231231233213
+                  </a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">
+                    Reserva do AirBnB
+                  </span>
+                  <a
+                    href="#"
+                    className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                  >
+                    https://www.airbnb.com.br/rooms/104700011123123213213123121231231231231231233213
+                  </a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            </div>
+
+            <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              <Plus className="size-5" />
+              Cadastrar novo link
+            </button>
+          </div>
+          <div className="w-full h-px bg-zinc-800" />
+          <div></div>
+        </div>
       </main>
     </div>
   );
